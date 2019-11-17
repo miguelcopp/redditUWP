2d083ff105ff62d79491d80d23591ccb9014402b
Miguel Coppola <miguelcopp@gmail.com>
2019-11-17 15:23:07 -0300

    Implementing the RedditSharp library

---
+++ RedditUWP/RedditUWP/RedditAPI.cs
@@ -0,0 +1,23 @@
+﻿using RedditSharp;
+using RedditSharp.Things;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditUWP
+{
+    class RedditAPI
+    {
+        public static IEnumerable<Post> GetTop()
+        {
+            var reddit = new Reddit();
+            var subreddit = reddit.FrontPage;
+
+            IEnumerable<Post> topPosts = subreddit.GetTop(FromTime.All).Take(50);
+
+            return topPosts;
+        }
+    }
+}
