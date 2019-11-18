81edc8129f69096344ce0f4d75787c8e372ee70b
Miguel Coppola <miguelcopp@gmail.com>
2019-11-17 22:57:16 -0300

    styles

--- RedditUWP/RedditUWP/StringFormatConverter.cs
+++ RedditUWP/RedditUWP/StringFormatConverter.cs
@@ -7,15 +7,23 @@ using Windows.UI.Xaml.Data;
 
 namespace RedditUWP
 {
+    // Returns the date formated in the format "x months ago"
     public class StringFormatConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            DateTime postDate = DateTime.Parse(value.ToString());
+            try
+            {
+                DateTime postDate = DateTime.Parse(value.ToString());
 
-            var diffDates = ((DateTime.Now.Year - postDate.Year) * 12) +  DateTime.Now.Month - postDate.Month;
+                var diffDates = ((DateTime.Now.Year - postDate.Year) * 12) + DateTime.Now.Month - postDate.Month;
 
-            return diffDates;
+                return diffDates;
+            }
+            catch(Exception)
+            {
+                return null;
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
